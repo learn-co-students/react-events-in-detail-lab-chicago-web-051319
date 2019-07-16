d786f80a38fe7405b770928b17023fcd7a59ce32
Joel Seng <joel29dec@gmail.com>
2019-07-16 09:50:47 -0500

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
-// Code DelayedButton Component Here
+// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+import { setTimeout } from 'timers';
+export default class DelayedButton extends Component {
+    
+      handleClick = (event) => {
+        const { onDelayedClick, delay } = this.props;
+        setTimeout(() => onDelayedClick(event), delay)
+      }
+
+    render() { 
+       
+        return ( 
+            <button onClick={this.handleClick}>Mouse Delay</button>
+         );
+    }
+}
+ 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 // These must be defined for code to work in browser
 import CoordinatesButton from './components/CoordinatesButton';
 import DelayedButton from './components/DelayedButton';
+function onDelayedClick(e){
+  console.log(e)
+}
 
 ReactDOM.render(
   <div>
     <CoordinatesButton onReceiveCoordinates={ mouseCoordinates => console.log(mouseCoordinates) } />
-    <DelayedButton onDelayedClick={ event => console.log(event) } delay={1500} />
+    <DelayedButton onDelayedClick={ onDelayedClick } delay={300} />
   </div>,
   document.getElementById('global')
 );
